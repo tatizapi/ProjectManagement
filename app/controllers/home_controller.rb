85f28005c18479c700a8807ab90992d30be1b082
tatizapi <tatiana.zapirtan@yahoo.com>
2018-07-17 13:26:27 +0300

    admin authentification added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-
+    redirect_to admin_index_path if current_user.admin
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  resources :admin, only: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
