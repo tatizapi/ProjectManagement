15833c501278542fc2c214a97662fad719d8abb6
tatizapi <tatiana.zapirtan@yahoo.com>
2018-07-18 10:47:54 +0300

    some validations to adding client form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
 
   scope :clients, -> { where(type: 'Client') }
 
+  validates :first_name, :last_name, :password, :username, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :username, length: { minimum: 3 }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
   def email_required?
     false
   end
