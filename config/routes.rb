1de3e9ad1ebcaffcae15ccd7c1f5e8772e23fa5f
tatizapi <tatiana.zapirtan@yahoo.com>
2018-07-18 13:54:02 +0300

    path to admin -> projects added

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   scope '/admin' do
     resources :clients
+    resources :projects
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
