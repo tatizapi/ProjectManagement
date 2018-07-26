bb8edbf14fbec9f56a545087a7d1e28d651eca3e
tatizapi <tatiana.zapirtan@yahoo.com>
2018-07-26 13:54:40 +0300

    tasks added to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
   def index
     @project = Project.find(params[:project_id])
+    @tasks_todo = Task.where(status: 'todo')
+    @tasks_inprogress = Task.where(status: 'inprogress')
+    @tasks_complete = Task.where(status: 'complete')
+    @tasks_done = Task.where(status: 'done')
   end
 
 end
