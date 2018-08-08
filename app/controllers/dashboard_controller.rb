9cff8e12371852f0e7ef1823b2f8a9ca126e00fc
tatizapi <tatiana.zapirtan@yahoo.com>
2018-08-08 15:11:11 +0300

    bug fixed, now admin can make a task go back

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -25,7 +25,7 @@ class DashboardController < ApplicationController
       params[:back] ? status = "todo" : status = "complete"
       status == "complete" ? task.update(status: status, completed_at: Time.now) : task.update(status: status)
     when "complete"
-      if params[:back] && current_user.is_developer(@project)
+      if params[:back] && (current_user.is_developer(@project) || current_user.type == "Admin" )
         status = "inprogress"
       elsif params[:back] && current_user.is_tester(@project)
         status = "todo"
