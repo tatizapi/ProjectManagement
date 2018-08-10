ceef9fc5ed9681f96960490529321c2422fa6d5d
tatizapi <tatiana.zapirtan@yahoo.com>
2018-08-10 11:58:16 +0300

    page doesn't refresh when adding a new comment (finally)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   before_action :get_current_task, only: [:index, :new, :create, :destroy]
+  before_action :get_task_comments, only: [:index, :create]
 
   def index
-    @comments = @task.comments
   end
 
   def new
@@ -12,7 +12,6 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params.merge(user_id: current_user.id, task_id: @task.id ))
     @comment.save
-    redirect_to project_dashboard_index_path(@task.project_id)
   end
 
   def destroy
@@ -25,6 +24,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body, {attachments: []})
+  end
+
+  def get_task_comments
+    @comments = @task.comments
   end
 
   def get_current_task
