ad772c5f0d778a77ef48732c3e53b0904f11f3fe
tatizapi <tatiana.zapirtan@yahoo.com>
2018-08-16 12:06:54 +0300

    resized messages and formatted date

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  def get_time
+    if created_at.today?
+      created_at.strftime("%H:%M")
+    elsif created_at == Date.yesterday
+      "yesterday"
+    else
+      created_at.strftime("%d %^b")
+    end
+  end
 end
