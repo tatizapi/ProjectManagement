953735dee49e76f463c60c6f63f90da81a0d1be5
tatizapi <tatiana.zapirtan@yahoo.com>
2018-08-17 16:36:03 +0300

    fixed date and autocomplete off on chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,7 +7,7 @@ class Message < ApplicationRecord
   def get_time
     if created_at.today?
       created_at.strftime("%H:%M")
-    elsif created_at == Date.yesterday
+    elsif created_at.to_date == Date.yesterday
       "yesterday"
     else
       created_at.strftime("%d %^b")
