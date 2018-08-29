8bc2bd008265116754442740312a6b2a19d58813
tatizapi <tatiana.zapirtan@yahoo.com>
2018-08-29 13:22:56 +0300

    guard against negative remaining time

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -71,16 +71,22 @@ class Ticket < ApplicationRecord
 
   def remaining_time(deadline)
     remaining_time = (deadline - Time.now).to_i #difference between two dates is in seconds
-    nr_of_days = remaining_time / 86400
-    nr_of_hours = (remaining_time % 86400) / 3600
-    nr_of_minutes = ((remaining_time % 86400) % 3600) / 60
-    "#{nr_of_days} days, #{nr_of_hours} hours, #{nr_of_minutes} minutes"
+    if remaining_time > 0
+      nr_of_days = remaining_time / 86400
+      nr_of_hours = (remaining_time % 86400) / 3600
+      nr_of_minutes = ((remaining_time % 86400) % 3600) / 60
+      "#{nr_of_days} days, #{nr_of_hours} hours, #{nr_of_minutes} minutes"
+    else
+      "you're done"
+    end
   end
 
   def remaining_time_precent(deadline)
     total_time = (deadline - created_at).to_i
     passed_time = (Time.now - created_at).to_i
     percent = (passed_time.to_f / total_time) * 100
-    sprintf('%.2f', percent)
+    if percent < 100
+      sprintf('%.2f', percent)
+    end
   end
 end
