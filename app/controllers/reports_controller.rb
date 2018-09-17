a2a0e819fc96a787bb44691d4d6a096375aa203c
tatizapi <tatiana.zapirtan@yahoo.com>
2018-09-17 09:10:42 +0300

    change buttons to tabs on dashboard page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -15,8 +15,8 @@ class ReportsController < ApplicationController
   def create
     report = Report.new(settings: report_params)
     report.settings['employees'].map!{ |e| Employee.find_by(first_name: e.split()[0], last_name: e.split()[1]).id } if report.settings['employees']
-    # ^ because in employees dropdown i only have employees's full names, not employees entities form DB
-
+    # ^ because in employees dropdown I only have employees's full names, not employees entities or ids
+    
     if report.save
       redirect_to project_report_path(@project, report)
     else
@@ -51,7 +51,7 @@ class ReportsController < ApplicationController
 
   def tickets_priority_piechart
     conditions = { project_id: @report.settings['projects'], employee_id: @report.settings['employees'], type: @report.settings['tickets'], status: @report.settings['statuses'] }
-    conditions.delete_if {|k, v| v.blank? }
+    conditions.delete_if { |k, v| v.blank? }
     tickets_by_priority = Ticket.where(conditions).group(:priority).count
     @nr_of_high_priority_tickets = tickets_by_priority['high']
     @nr_of_medium_priority_tickets = tickets_by_priority['medium']
