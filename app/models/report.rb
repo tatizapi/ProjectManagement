81b2148dbd4119ecb00ddf5d6d5968364019e921
tatizapi <tatiana.zapirtan@yahoo.com>
2018-10-03 17:51:10 +0300

    template is set for dropdowns

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,4 +5,15 @@ class Report < ApplicationRecord
   belongs_to :project
 
   #validates_presence_of :title, :if => :save_as_template?
+  def from_ids_to_full_names
+    full_names = []
+
+    unless settings['employees'].nil?
+      settings['employees'].each do |employee_id|
+        full_names.push(Employee.find(employee_id).full_name)
+      end
+    end
+
+    full_names
+  end
 end
